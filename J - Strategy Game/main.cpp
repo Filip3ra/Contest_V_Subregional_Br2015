9898b54521d85625ecad459e21c84d95366df319
Filip3ra <filipimacielrj@gmail.com>
2022-03-21 19:45:38 -0300

    questão iniciada

---
+++ J - Strategy Game/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    int J, R;
+
+    while(cin >> J >> R){
+
+        int fim = (J*R);
+        int cont = 0;
+
+        for(int i = 0; i < fim; ++i){
+            
+            if(cont<J)
+                cont++;
+
+            
+
+
+        }
+
+    }
+
+    return 0;
+}
