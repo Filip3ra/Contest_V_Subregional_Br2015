a2452a2af3685f4ac3063a9c9461a7a6ee432d99
Filip3ra <filipimacielrj@gmail.com>
2022-03-22 09:16:11 -0300

    bug na leitura da lista de pontos

--- J - Strategy Game/main.cpp
+++ J - Strategy Game/main.cpp
@@ -1,25 +1,53 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char const *argv[]){
 
     int J, R;
+    int jogadores[502]{0}; // declara já preenchido com zeros
+    string str;
 
-    while(cin >> J >> R){
+    while(cin >> J >> R){   // jogadores e rounds
 
-        int fim = (J*R);
-        int cont = 0;
+        getline(cin, str);  // lista de pontos
+
+        int fim = (J*R);    
+        int cont = 0;       // contador pra cada rodada
+        int p;              // pontos
 
         for(int i = 0; i < fim; ++i){
-            
-            if(cont<J)
-                cont++;
 
-            
+            if(str[i] != ' '){
 
+                p = (int)str[i]; // <-- BUG, PARECE QUE TA LENDO O VALOR INT CHAR DO CARACTERE 
 
+                cout << p << endl;
+
+                if(cont<J){
+                    jogadores[cont] += p;
+                    
+                    cont++;
+                }
+                else{ // se entrei no else, uma rodada acabou
+                    cont = 0;
+                }
+            }
         }
+        str.clear();
+
+        int maior = 0 ;
+        int vencedor = 0;
+
+        for(int i = 0; i < J ; ++i){ // descobre quem é o vencedor
+            if(jogadores[i]>=maior){
+                maior = jogadores[i];
+                vencedor = i;
+            }
+        }
+
+        cout << vencedor << endl;
 
     }
 
